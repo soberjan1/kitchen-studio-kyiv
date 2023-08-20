d0a3f45b247f2bbb3bbd5af3212b3236b7d0204c
soberjan1 <aliguseynov48@gmail.com>
2023-08-20 16:16:25 +0300

    .

---
+++ src/js/hero.js
@@ -0,0 +1,10 @@
+// import Swiper from 'swiper/bundle';
+
+// import 'swiper/css/bundle';
+
+// const swiper = new Swiper('.swiper', {
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+// });
